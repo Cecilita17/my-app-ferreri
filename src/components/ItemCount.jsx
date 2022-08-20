79cbbb79354363abdfc338d748b07423945eafbd
Cecilia <ceciferreri@gmail.com>
2022-08-20 17:46:36 -0300

    desafio contador agregado onAdd

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -4,7 +4,7 @@ const ItemCount = ({stock, initial, onAdd}) => {
     const [count, setCount] = useState(initial);
 
     const add = ()=>{
-        count > 0 && setCount(count - 1)
+        count > initial && setCount(count - 1)
     };
     
     const subtract = ()=>{
@@ -12,12 +12,17 @@ const ItemCount = ({stock, initial, onAdd}) => {
     };
 
     return (
-        <div style={{ border: "1px solid white", backgroundColor: "grey" , margin: "30px", padding: "5px", width: "100px", height: "60px"}}>
+        <div style={{ border: "1px solid white", backgroundColor: "grey" , margin: "30px", padding: "5px", width: "100px", height: "70px"}}>
             <div>{count}</div>
             <button onClick={add} style={{width:"30px", height:"20px", backgroundColor:"brown", color:"white", border:"none"}}>-</button>
-            <button onClick={subtract} style={{width:"30px", height:"20px", backgroundColor:"brown", color:"white", border:"none"}}>+</button>
+            <button onClick={subtract} style={{ width: "30px", height: "20px", backgroundColor: "brown", color: "white", border: "none" }}>+</button>
+            <div><button onClick={() => onAdd(count)}>Add</button></div>
         </div>
     )
+
+    
 };
 
 export default ItemCount
+
+{/* <button onClick={()=>{setCount(count-1)}}disabled={count === initial}></button> */}
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ItemCount from './ItemCount'
 
 const ItemListContainer = (props) => {
+  function onAdd(count) {
+    console.log("soy onAdd y el valor del contador es:" , count );
+  }
   return (
     <div className={props.clase}> {props.msg}
-    <ItemCount stock={7} initial = {1} onAdd={null} />
+    <ItemCount stock={7} initial = {1} onAdd={onAdd} />
     </div>
   )
 }
